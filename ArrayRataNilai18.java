ac5362c0b90275ead39225f4ed38f65373c84044
Nabila Ardianti Lunetta <nabilaardianti17@gmail.com>
2025-11-12 10:15:06 +0700

    Modifikasi 1 percobaan 3

--- ArrayRataNilai18.java
+++ ArrayRataNilai18.java
@@ -4,12 +4,16 @@ public class ArrayRataNilai18 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] nilaiMhs = new int[10];
+        int jmlLulus = 0;
         double total = 0;
         double rata2;
 
         for (int i = 0 ; i < nilaiMhs.length ; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
             nilaiMhs[i] = sc.nextInt();
+            if (nilaiMhs[i] > 70) {
+                jmlLulus++;
+            }
         }
 
         for (int i = 0 ; i < nilaiMhs.length ; i++) {
@@ -18,5 +22,6 @@ public class ArrayRataNilai18 {
 
         rata2 = total/nilaiMhs.length;
         System.out.println("Rata - rata nilai = "+rata2);
+        System.out.println("Jumlah mahasiswa lulus : "+jmlLulus);
     }
 }
