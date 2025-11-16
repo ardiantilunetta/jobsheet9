554789d3065c2ec896b059423a451c774f019f90
Nabila Ardianti Lunetta <nabilaardianti17@gmail.com>
2025-11-17 01:00:55 +0700

    deklarasi variabel

---
+++ Tugas3SearchMenu18.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Tugas3SearchMenu18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String menu[] = {"Nasi goreng", "Mie goreng", "Roti bakar", "Kentang goreng", "Teh tarik", "Cappucino", "Chocolate ice"};
+        String cari;
+        
+    }
+}
