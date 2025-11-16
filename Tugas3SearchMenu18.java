ca7c7ade1a5be0d5ed7fe89cb00550009666dd68
Nabila Ardianti Lunetta <nabilaardianti17@gmail.com>
2025-11-17 01:28:17 +0700

    Proses pencarian

--- Tugas3SearchMenu18.java
+++ Tugas3SearchMenu18.java
@@ -5,6 +5,17 @@ public class Tugas3SearchMenu18 {
         Scanner sc = new Scanner(System.in);
         String menu[] = {"Nasi goreng", "Mie goreng", "Roti bakar", "Kentang goreng", "Teh tarik", "Cappucino", "Chocolate ice"};
         String cari;
+        boolean hasil;
+
+        System.out.print("Menu yang dicari : ");
+        cari = sc.nextLine();
+        
+        for (int i = 0 ; i < 7 ; i++) {
+            if (cari.equalsIgnoreCase(menu[i])) {
+                hasil = true;
+            }
+        }
+
         
     }
 }
