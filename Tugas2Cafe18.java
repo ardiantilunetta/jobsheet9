0f470e168c79e14b8ab7e66031a7d508137d02b3
Nabila Ardianti Lunetta <nabilaardianti17@gmail.com>
2025-11-17 00:49:52 +0700

    tampil data pesanan

--- Tugas2Cafe18.java
+++ Tugas2Cafe18.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Tugas2Cafe18 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int jmlpsn, totalbiaya;
+        int jmlpsn, totalbiaya=0;
 
         System.out.print("Masukkan jumlah pesanan : ");
         jmlpsn = sc.nextInt();
@@ -12,11 +12,16 @@ public class Tugas2Cafe18 {
         int harga[] = new int[jmlpsn];
 
         for (int i = 0 ; i < jmlpsn ; i++) {
+            sc.nextLine();
             System.out.print("Masukkan nama makanan/minuman  = ");
             nama[i] = sc.nextLine();
-            System.out.print("Masukkan harga makanan/minuman = ");
+            System.out.print("Harga = ");
             harga[i] = sc.nextInt();
-            
+        }
+
+        System.out.println("---- DAFTAR PESANAN ----");
+        for (int i = 0 ; i < jmlpsn ; i++) {
+            System.out.println(nama[i]+"\t\t"+harga[i]);
         }
     }
 }
